1b8274775a2c092ca0e29305483e9a383dbadbfb
fakukpg <faku_kpg@hotmail.com>
2017-09-23 12:16:14 -0300

    mkdir

--- FileSystem/funcionesConsolaFS.c
+++ FileSystem/funcionesConsolaFS.c
@@ -20,6 +20,7 @@
 #include "fileSystem.h"
 #include "errno.h"
 #include "commons/string.h"
+#include "commons/collections/queue.h"
 
 
 void format(){
@@ -57,16 +58,24 @@ int mkdir(char* path_dir){
 		}
 
 		//Verifico que el file no exista en la tabla de directorios
-		int existe = 1;
+		int existe = 0;
 		int i = 0;
 
-		if(!existe == 0){
+		if(existe == 0){
 
 			while(!tabla_Directorios[i].nombre == fname){
 				i++;
 			}
-		//Obtengo nombre del padre y busco su index en la tabla
+			existe = 1;
+		//enviar mensaje a quien corresponda de que el directorio ya existe
 		}
+
+		//Obtengo el nombre del padre
+			char *dpath = getPathPadre(path_dir);
+			dupPath = strdup(dpath);
+			char *dname = getNombreFile(dupPath);
+			free(dupPath);
+		//Obtener datos padre y asignar todo a la tabla
 }
 
 void cpfrom(char* path_archivo_origen, char* directorio_yamafs){
@@ -121,5 +130,32 @@ char *getNombreFile(char *path){
 	return nombreFile;
 }
 
+char *getPathPadre(char *path){
+
+	char** subpath = string_split(path, "/");
+
+	char *pathPadre = string_new();
+	char *subpathPadre = string_new();
+	t_queue *colaPathPadre = queue_create();
+
+	void closure(char *value){
+		queue_push(colaPathPadre, value);
+	}
+	string_iterate_lines(subpath, closure);
+
+	while(queue_size(colaPathPadre)>1){
+		char *value = queue_pop(colaPathPadre);
+		string_append_with_format(&subpathPadre,"%s/", value);
+		free(pathPadre);
+		pathPadre = string_substring_until(subpathPadre, string_length(subpathPadre) - 1);
+	}
+
+	free(subpathPadre);
+	queue_destroy_and_destroy_elements(colaPathPadre, free);
+
+	free(subpath);
+
+	return pathPadre;
+}
 
 
