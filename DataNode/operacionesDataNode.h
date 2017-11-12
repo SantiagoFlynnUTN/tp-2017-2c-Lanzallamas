c7c33458a9e3e99753b94c6231925a6a7ef8f092
santiagoflynnutn <santiago.flynn2@gmail.com>
2017-11-11 22:18:17 -0300

    const MB

--- DataNode/operacionesDataNode.h
+++ DataNode/operacionesDataNode.h
@@ -1,8 +1,8 @@
 #ifndef OPERACIONESDATABIN_H
 #define OPERACIONESDATABIN_H
 
-#define MB 1048576
-//#define MB 50
+//#define MB 1048576
+#define MB 50
 
 int getBloque(int bloque, char data[MB]);
 int setBloque(int bloque, char data[MB]);
