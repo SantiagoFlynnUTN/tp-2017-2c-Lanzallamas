98b7f9d927106ac5a4f21a547c90a502c4e7a322
Msevillano <sevillano.mauro@gmail.com>
2017-11-20 16:34:47 -0300

    Script Z-Commons

---
+++ scripts/Z-Com-Script.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+echo "Copiando Repo Git"
+
+git clone "https://github.com/sisoputnfrba/tp-2017-2c-Lanzallamas.git"
+cd tp-2017-2c-Lanzallamas
+
+echo "Instalando/Compilando bibliotecas"
+
+git clone "https://github.com/sisoputnfrba/so-commons-library"
+cd so-commons-library
+sudo make install
+cd ..
+
+cd Z-Commons
+
+gcc -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"archivos.d" -MT"archivos.d" -o "archivos.o" archivos.c
+gcc -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"cosas.d" -MT"cosas.d" -o "cosas.o" cosas.c
+gcc -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"prueba.d" -MT"prueba.d" -o "prueba.o" prueba.c
+gcc -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"sockets.d" -MT"sockets.d" -o "sockets.o" sockets.c
+gcc -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"structUtiles.d" -MT"structUtiles.d" -o "structUtiles.o" structUtiles.c
+gcc -shared -o "libZ-Commons.so"  ./archivos.o ./cosas.o ./prueba.o ./sockets.o ./structUtiles.o
+
+exit 0
