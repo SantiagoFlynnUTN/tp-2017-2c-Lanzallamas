9076a2dba101c9e5d7def1212863829d55f73074
santiagoflynnutn <santiago.flynn2@gmail.com>
2017-11-27 03:40:27 -0300

    msg_peek

--- FileSystem/servidor.c
+++ FileSystem/servidor.c
@@ -44,10 +44,15 @@ void manejarCliente(int newfd){
     int count;
     ioctl(newfd, FIONREAD, &count);
 
-    if(count == 0){
-
-        return;
-    }
+	if (count == 0) {
+		if (recv(newfd, &buf, sizeof(int), MSG_PEEK) == 0) {
+			log_debug(logger, "selectserver: socket %d hung up\n", newfd);
+			marcarNodoDesconectado(newfd);
+			close(newfd); // bye!
+			FD_CLR(newfd, &master); // eliminar del conjunto maestro
+		}
+	return;
+}
 
 
     numbytes = recv(newfd, &buf, sizeof(int), 0); //leo el primer byte. Me dirá el tipo de paquete. (es un int)
