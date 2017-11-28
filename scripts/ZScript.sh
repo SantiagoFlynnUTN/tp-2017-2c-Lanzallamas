1d20be507d6c337bfb4a070612941a0263f31e6b
santiagoflynnutn <santiago.flynn2@gmail.com>
2017-11-28 15:43:04 -0300

    script papoteado

---
+++ scripts/ZScript.sh
@@ -0,0 +1,92 @@
+#!/bin/sh
+echo "Instalando/Compilando bibliotecas"
+
+git clone "https://github.com/sisoputnfrba/so-commons-library"
+cd so-commons-library
+sudo make install
+cd ..
+
+cd ..
+cd Z-Commons/Debug
+
+make clean
+make
+
+cd ..
+cd ..
+
+echo "Compilando DataNode"
+
+cd DataNode/Debug
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/utnso/workspace/tp-2017-2c-Lanzallamas/Z-Commons/Debug
+
+make clean
+make
+
+echo "Generando data.bin"
+
+fallocate -l 50M /home/utnso/data.bin
+
+echo "Listo DataNode"
+cd ..
+cd ..
+
+sudo apt-get install libreadline6 libreadline6-dev
+
+echo "Compilando FileSystem"
+
+cd FileSystem/Debug
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/utnso/workspace/tp-2017-2c-Lanzallamas/Z-Commons/Debug
+
+make clean
+make
+
+echo "Listo FileSystem"
+cd ..
+cd ..
+
+echo "Compilando Master"
+
+cd Master/Debug
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/utnso/workspace/tp-2017-2c-Lanzallamas/Z-Commons/Debug
+
+make clean
+make
+
+echo "Listo Master"
+cd ..
+cd ..
+
+echo "Compilando Worker"
+
+cd Worker/Debug
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/utnso/workspace/tp-2017-2c-Lanzallamas/Z-Commons/Debug
+
+make clean
+make
+
+echo "Listo Worker"
+cd ..
+cd ..
+
+echo "Compilando YAMA"
+
+cd YAMA/Debug
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/utnso/workspace/tp-2017-2c-Lanzallamas/Z-Commons/Debug
+
+make clean
+make
+
+echo "Listo YAMA"
+cd
+vim .bashrc
+
+export LC_ALL=C
+#./Debug/DataNode
+
+exit 0
