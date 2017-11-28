75300c11bccd7336a502cc6d5de53a015a56621e
SantiagoFlynnUTN <santiago.flynn2@gmail.com>
2017-11-28 09:54:28 -0300

    Update YAMA-Script.sh

--- scripts/YAMA-Script.sh
+++ scripts/YAMA-Script.sh
@@ -19,7 +19,7 @@ echo "Compilando YAMA"
 
 cd YAMA/Debug
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/utnso/workspace/Z-Commons/Debug
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/utnso/workspace/tp-2017-2c-Lanzallamas/Z-Commons/Debug
 
 make clean
 make
